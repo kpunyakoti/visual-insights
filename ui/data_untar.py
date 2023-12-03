bd3c7f4946f5d15b69cbdad038377b9449616d4f
krishnaPunyakoti <krishna@lifetimely.io>
2023-12-03 19:14:25 +0530

    updated untar

--- ui/data_untar.py
+++ ui/data_untar.py
@@ -1,12 +1,15 @@
 import tarfile
 import time
+import os
 
 # Check if .tgz file exists under data directory path.
 # For project demo purposes, it will check only for nuScenes_CamFront_resized.
-target_path = data_directory_path + 'nuScenes_CamFront_resized'
+data_directory_path = 'data'
+os.chdir("../")
+target_path = os.path.join(data_directory_path,'nuScenes_CamFront_resized')
 if not os.path.exists(target_path):
     tgz_filename = 'nuScenes_CamFront_resized.tgz'
-    tgz_filepath =  data_directory_path + tgz_filename
+    tgz_filepath =  os.path.join(data_directory_path,tgz_filename)
     if os.path.exists(tgz_filepath):
         with tarfile.open(tgz_filepath, 'r:gz') as tar:
             tar.extractall(data_directory_path)
